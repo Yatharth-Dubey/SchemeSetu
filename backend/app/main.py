4df5edf29bf646ca8ec0d93b466cf3285d4011a4
Yatharth-Dubey <yatharthdubey12dec2004@gmail.com>
2026-06-18 20:59:19 +0530

    chat route

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from app.api.routes.chat import router
+
 app = FastAPI(
     title = "SchemeSetu API"
+)
+
+app.include_router(
+    router,
+    prefix="/api"
 )
 
 @app.get("/")
